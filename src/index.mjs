81ffa6145950a7374dbe8e54ae6b27295f54797f
Jascha <jascha@jaeh.at>
2019-05-20 07:07:37 +0200

    format

--- src/index.mjs
+++ src/index.mjs
@@ -71,4 +71,3 @@ Item.propTypes = [
 ]
 
 const GitList = { Item }
-
--- test/spec.mjs
+++ test/spec.mjs
@@ -1,4 +1,6 @@
 import { is } from '@magic/test'
 import * as GitList from '../src/index.mjs'
 
-export default [{ fn: () => GitList.View, expect: is.function, info: 'expect GitList.View to be a function' }]
+export default [
+  { fn: () => GitList.View, expect: is.function, info: 'expect GitList.View to be a function' },
+]
